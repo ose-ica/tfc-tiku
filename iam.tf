5c1e857aeca3ee8065ac5b4c0f0cc9677b8caf76
getreadyforthecode <99776759+getreadyforthecode@users.noreply.github.com>
2022-06-05 17:01:49 -0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,5 +1,8 @@
-resource "aws_iam_user" "of" {
-  name = osetikufred
-  path =  "/"
-  
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
 }
